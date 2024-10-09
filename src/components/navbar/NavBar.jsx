f9dc9cd3f66e67315eaefaddf91f25582b50f0d3
KummaraHariBabu <kummaraharibabu8@gmail.com>
2024-10-09 15:42:41 +0530

    created navbar component as well as added styles to this component

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
-import "./navbar.css";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./navbar.scss";
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [show, setShow] = useState(false);
+  const toggleMenu = () => {
+    setShow(!show);
+  };
+  return (
+    <nav className="nav-wrapper">
+      <div>
+        <Link to="/">
+          <i>{"<OAK/>"}</i>
+          <span>Recipe</span>
+        </Link>
+      </div>
+      <div className={show ? "link-wrapper show" : "link-wrapper"}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="login">Login</NavLink>
+        <NavLink to="about">About</NavLink>
+      </div>
+      <div className="burger-menu" onClick={toggleMenu}>
+        &#9776;
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
