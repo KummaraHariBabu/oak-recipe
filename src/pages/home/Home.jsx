bc5bd16328e7abf14e473efc5d1c1ee723babe2c
KummaraHariBabu <kummaraharibabu8@gmail.com>
2024-10-09 20:07:22 +0530

    header component has done as well as added header styles to this

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,14 +5,18 @@ import Header from "../../components/header/Header";
 import Card from "../../components/card/Card";
 
 const Home = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("egg");
   const [selectedMeal, setSelectedMeal] = useState("breakfast");
   const [recipes, setRecipes] = useState([]);
 
+  //mealtypes 
+  const mealTypes = ["Breakfast","Lunch","Dinner","Snacks","Tea Time"]
   const appId = process.env.REACT_APP_API_ID;
   const appKey = process.env.REACT_APP_API_KEY;
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&mealType=${selectedMeal}`;
+  const url = `https://api.edamam.com/search?q=${query}&
+  app_id=${appId}&app_key=${appKey}&mealType=${selectedMeal}
+  `;
 
   const getData = async () => {
     try {
@@ -23,15 +27,21 @@ const Home = () => {
     }
   };
   console.log(recipes);
-  useEffect(() => {
-    getData();
-  }, []); // Add dependency array to avoid infinite requests
+//   useEffect(() => {
+//     getData();
+//   }, []); // Add dependency array to avoid infinite requests
 
   return (
     <div>
-        <Header />
-        <Card />
-
+      <Header
+        query={query}
+        setQuery={setQuery}
+        selectedMeal={selectedMeal}
+        setSelectedMeal={setSelectedMeal}
+        mealTypes={mealTypes}
+        getData={getData}
+      />
+      <Card />
     </div>
   );
 };
